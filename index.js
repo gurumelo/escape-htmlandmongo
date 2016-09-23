051616d5cf3af12d77a45d5a57e32216cfcf6479
mari <mari@debians>
2016-09-23 12:08:22 +0200

    Esc

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ var esc = function(lacadena) {
 	return lacadena.replace(/&/g, '&amp;').replace(/^\$/, '&#36;').replace(/^./, '&#46;').replace(/"/g, '&quot;').replace(/'/g, '&#x27;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/\//g, '&#x2F;').replace(/`/g, '&#96;');
 }
 
-module.exports = esc;
+module.exports.esc = esc;
